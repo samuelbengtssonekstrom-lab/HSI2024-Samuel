192a639f75c746b969f888254cdc54943a4f23f8
Samuel <samuel.bengtsson.ekstrom@gmail.com>
2025-08-20 21:55:24 +0200

    Done Playing

--- HSI2024/code-test.jl
+++ HSI2024/code-test.jl
@@ -1,21 +1,48 @@
 # %%
-  function orbit(T, x0, N)
-     orb = zeros(typeof(x0), N)
-     x = x0
-     for i in 1:N
-        orb[i] = x
-        x = T(x)
-     end
-     return orb
-  end
-
-# %%
+  using Random
   using Plots
-  T(x) = mod(1/x,1)
-  @time v = orbit(T, 1/π, 1000000)
-  plt = histogram(v, normalize = :pdf, label = "Empirical", bins = 100)
-  plt
+
+function GaussMap(x)
+    if x == 0
+        return 0
+    else
+        return mod(1/x, 1)
+    end
+end
+
+function ContFracCoeff(x,n)
+    coeffs = zeros(n)
+
+    # 1/T^{n-1}(x)- T^n(x)
+    
+    for i in 1:n
+      if x == 0
+        break
+      end
+        coeffs[i] = 1/x - GaussMap(x)
+        x = GaussMap(x)
+    end
+    return coeffs
+end
+
+function approx_from_frac(coeffs)
+    n = length(coeffs)
+    temp = 0
+    for i in n:-1:1
+      if coeffs[i] == 0
+      else
+        temp = coeffs[i] + 1/temp
+      end
+    end
+    return 1/temp    
+end
 
 # %%
-  g(x) = 1/(log(2)*(1+x))
-  plot!(plt, g, 0, 1, color=:orange, label = "Density")
+    #Random.seed!(1234)
+    n = 10
+    x = rand()
+    println("x equals: ", x)
+    coeffs = ContFracCoeff(x, n)
+
+    approx = approx_from_frac(coeffs)
+    println("Approximation from continued fraction: ", approx)
