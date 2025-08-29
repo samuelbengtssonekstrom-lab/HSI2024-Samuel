141927244e452608486aba7621c3368808c6e321
Samuel <samuel.bengtsson.ekstrom@gmail.com>
2025-08-29 12:01:06 +0200

    No idea, stochastic stuff

--- Testing/.vscode/Logistic-Maps.jl
+++ Testing/.vscode/Logistic-Maps.jl
@@ -1,10 +1,11 @@
+using Plots
 function orbit(x_0, T, n)
     x = x_0
     orbit_points = zeros(typeof(x),n)
     orbit_points[1] = x_0
-    eps = 1e-2
+    eps = 1e-1
     for i in 1:n-1
-        noise = eps * sin(2π * rand())
+        noise = eps * sin(2π * rand())^3
         x = T(x) 
         x = test(x, noise)
         orbit_points[i+1] = x
@@ -12,13 +13,28 @@ function orbit(x_0, T, n)
     return orbit_points
 end
 
+function multiOrbit(x_0, T, n)
+    dim = length(x_0)
+    v = zeros(typeof(x_0[1]), dim, n)
+    v[:,1] = x_0
+    T_1(x) = mod(2x,1)
+    T_2(x) = mod(1-3x^2,1)
+    M = [T_1, T_2]
+    for j in 2:n
+        for i in 1:dim
+            v[i, j] += mod(M[i](v[i, j-1]) + mean(v[:,j-1]),1)
+        end
+    end
+    return v
+end
+
 function main()
-    eps = 1e-2
-    T(x) = 4*x*(1 - x)
-    x_0 = rand()
-    n = 1_000_000
-    orbit_points = orbit(x_0, T, n)
-    histogram!(orbit_points, bins= n÷10000, title="Histogram of Orbit Points", xlabel="x", ylabel="Frequency")
+    T(x) = mod(1-2x^2,1)
+    n = 1_000
+    dim = 2
+    x_0 = rand(dim)
+    orbit_points = multiOrbit(x_0, T, n)
+    histogram(orbit_points', bins= 10, title="Histogram of Orbit Points", xlabel="x", ylabel="Frequency")
 end
 
 function test(x, noise)
